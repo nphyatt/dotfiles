6ed8983ca2136c6c3099ee623d4240799e814828
Nick Hyatt <nick@thrustack.com>
2026-04-13 09:55:04 -0500

    exclude noisy directories from our live grep hidden/gitignored search

--- nvim/lua/core/plugin_config/telescope.lua
+++ nvim/lua/core/plugin_config/telescope.lua
@@ -6,5 +6,11 @@ vim.keymap.set('n', '<leader>b', builtin.buffers, {})
 vim.keymap.set('n', '<leader>o', builtin.lsp_document_symbols, {})
 vim.keymap.set('n', '<leader>/', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>.', function()
-  builtin.live_grep({ additional_args = { '--hidden', '--glob', '!.git/' } })
+  builtin.live_grep({ additional_args = {
+    '--hidden', '--no-ignore',
+    '--glob', '!.git/',
+    '--glob', '!node_modules/',
+    '--glob', '!dist/',
+    '--glob', '!build/',
+  } })
 end)
